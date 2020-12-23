5bf975caa5ffdb6be04fd917b1f6f4fd245ec18a
ktakkun <finaltatsuyakun6@gmail.com>
2020-12-22 18:37:21 -0800

    feature: add users page

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { User } from 'src/app/interfaces/User';
+import { ApiService } from 'src/app/services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
+  dataSource: MatTableDataSource<User>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  displayedColumns = ['username', 'email', 'actions'];
+
+  userSubscription: Subscription;
+
+  users$ = this.apiService.getUsers().pipe(
+    map((res: any) =>
+      res.users.map((el: any) => {
+        return {
+          username: el.username,
+          email: el.email,
+        };
+      })
+    ),
+    tap((res: User[]) => {
+      this.dataSource = new MatTableDataSource<User>(res);
+      this.dataSource.paginator = this.paginator;
+    })
+  );
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.userSubscription = this.users$.subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 
 }
