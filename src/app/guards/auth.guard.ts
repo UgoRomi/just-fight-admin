6e373993b818f458b46dc8dac0a24a858c1b66da
ktakkun <finaltatsuyakun6@gmail.com>
2020-12-22 18:37:33 -0800

    refactor: rename auth.guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,7 +7,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
+export class AuthGuard implements CanActivate {
   constructor(private router: Router, private apiService: ApiService) {}
 
   canActivate(
