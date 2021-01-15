dc9e356f999693e9603b47cc042d8cf59baf847a
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-15 06:10:29 -0800

    feat: add auth guard to dashboard route

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,44 +1,55 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { ApiService } from '../services/api.service';
 import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private apiService: ApiService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log('body', this.router.getCurrentNavigation().extras.state);
-
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     if (localStorage.getItem('token')) return true;
-    
+
     const body = this.router.getCurrentNavigation().extras.state;
 
     if (!body || !body.email || !body.password) {
       this.router.navigate(['/login']);
       return false;
     }
-    
-    return this.apiService.login(body as {email: string, password: string}).pipe(
-      tap((response: { token: string }) => {
-        localStorage.setItem('token', response.token);
-      }),
-      map((response: { token: string }) => {
-        if (response.token) {
-          return true;
-        }
-        // this.router.navigate(['/dashboard']);
-        return false;
-      }),
-      catchError((error) => {
-        // this.router.navigate(['/login']);
-        return of(false);
-      })
-    );
+
+    return this.apiService
+      .login(body as { email: string; password: string })
+      .pipe(
+        tap((response: { token: string }) => {
+          localStorage.setItem('token', response.token);
+        }),
+        map((response: { token: string }) => {
+          // TODO: return !!response.token;
+          if (response.token) {
+            return true;
+          }
+          return false;
+        }),
+        catchError((error) => {
+          console.error(error);
+          return of(false);
+        })
+      );
   }
 }
