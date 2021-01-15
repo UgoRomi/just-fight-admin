46b2a4f541cc0469471feaceaef64ede4186081f
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-15 06:32:44 -0800

    refactor: remove subscription from user component

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,33 +1,22 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
-import { User } from 'src/app/interfaces/User';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { User } from 'src/app/interfaces/user.model';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit, OnDestroy {
+export class UsersComponent implements OnInit {
   dataSource: MatTableDataSource<User>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   displayedColumns = ['username', 'email', 'actions'];
 
-  userSubscription: Subscription;
-
   users$ = this.apiService.getUsers().pipe(
-    map((res: any) =>
-      res.users.map((el: any) => {
-        return {
-          username: el.username,
-          email: el.email,
-        };
-      })
-    ),
     tap((res: User[]) => {
       this.dataSource = new MatTableDataSource<User>(res);
       this.dataSource.paginator = this.paginator;
@@ -36,12 +25,5 @@ export class UsersComponent implements OnInit, OnDestroy {
 
   constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
-    this.userSubscription = this.users$.subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.userSubscription.unsubscribe();
-  }
-
+  ngOnInit(): void {}
 }
