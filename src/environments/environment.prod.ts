2094c0e19f08e1ac0e43fa3d516103cf871edce2
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-15 07:53:52 -0800

    chore: update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,41 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://for-the-players.herokuapp.com',
+  links: [
+    {
+      route: 'users',
+      label: 'Users',
+      icon: 'people',
+    },
+    // {
+    //   route: 'teams',
+    //   label: 'Teams',
+    //   icon: 'store',
+    // },
+    {
+      route: 'rulesets',
+      label: 'Rulesets',
+      icon: 'rule',
+    },
+    {
+      route: 'tournaments',
+      label: 'Tournaments',
+      icon: 'account_tree',
+    },
+    {
+      route: 'platforms',
+      label: 'Platforms',
+      icon: 'videogame_asset',
+    },
+    {
+      route: 'games',
+      label: 'Games',
+      icon: 'games',
+    },
+    // {
+    //   route: 'tickets',
+    //   label: 'Tickets',
+    //   icon: 'addchart',
+    // },
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,45 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'http://localhost:3000',
+  links: [
+    {
+      route: 'users',
+      label: 'Users',
+      icon: 'people',
+    },
+    // {
+    //   route: 'teams',
+    //   label: 'Teams',
+    //   icon: 'store',
+    // },
+    {
+      route: 'rulesets',
+      label: 'Rulesets',
+      icon: 'rule',
+    },
+    {
+      route: 'tournaments',
+      label: 'Tournaments',
+      icon: 'account_tree',
+    },
+    {
+      route: 'platforms',
+      label: 'Platforms',
+      icon: 'videogame_asset',
+    },
+    {
+      route: 'games',
+      label: 'Games',
+      icon: 'games',
+    },
+    {
+      route: 'tickets',
+      label: 'Tickets',
+      icon: 'addchart',
+    },
+  ],
 };
 
 /*
