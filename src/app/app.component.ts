4e3c5bf54940362b727fa10aac093beb94d3d63e
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-15 11:24:26 -0800

    refactor: change shown project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'just-fight-admin';
+  title = 'for-the-players-admin';
 }
