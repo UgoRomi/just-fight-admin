40e896b8b1f41db7a87ce917a12ed446b845b89f
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-18 03:52:26 -0800

    feat: add ticket component

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
-  styleUrls: ['./tickets.component.scss']
+  styleUrls: ['./tickets.component.scss'],
 })
 export class TicketsComponent implements OnInit {
+  tickets$ = this.apiService.getTickets();
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
