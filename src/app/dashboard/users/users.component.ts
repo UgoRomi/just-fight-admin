f9b494b37278165cd19fed189c2412ce42daa68b
ktakkun <finaltatsuyakun6@gmail.com>
2021-01-18 03:54:09 -0800

    feat: add verify user button

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { catchError, switchMap, tap } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/interfaces/user.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -16,14 +17,27 @@ export class UsersComponent implements OnInit {
 
   displayedColumns = ['username', 'email', 'actions'];
 
-  users$ = this.apiService.getUsers().pipe(
-    tap((res: User[]) => {
-      this.dataSource = new MatTableDataSource<User>(res);
-      this.dataSource.paginator = this.paginator;
-    })
-  );
+  initPaginator = tap((res: User[]) => {
+    this.dataSource = new MatTableDataSource<User>(res);
+    this.dataSource.paginator = this.paginator;
+  });
+
+  users$ = this.apiService.getUsers().pipe(this.initPaginator);
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {}
+
+  verifyUser(user: User) {
+    const patchedUser = { ...user };
+    patchedUser.status = 'VERIFIED';
+
+    this.users$ = this.apiService.patchUser(patchedUser).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(error);
+      }),
+      switchMap(() => this.apiService.getUsers().pipe(this.initPaginator))
+    );
+  }
 }
